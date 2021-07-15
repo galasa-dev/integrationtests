c6c74f233d3b8fc5d5cbd68b275c061c517815fd
Matthew Chivers <matthewchivers@outlook.com>
2021-07-15 14:00:41 +0100

    Added a quick TODO
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/AbstractCompilationLocal.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/AbstractCompilationLocal.java
@@ -7,12 +7,36 @@ package dev.galasa.inttests.compilation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.file.Path;
+
+import dev.galasa.BeforeClass;
 import dev.galasa.Test;
 import dev.galasa.galasaecosystem.IGenericEcosystem;
 import dev.galasa.java.ubuntu.IJavaUbuntuInstallation;
 import dev.galasa.linux.ILinuxImage;
 
 public abstract class AbstractCompilationLocal {
+	
+	/*
+	 * TODO
+	 * [ ] Download Simplatform
+	 * [ ] Unzip Simplatform
+	 * [ ] Move Simplatform files & code to working directory
+	 * [ ] Managers - Change Prefix
+	 * [ ] Tests - Change Prefix
+	 * Zip:
+	 * 	   [ ] Managers - Add PluginManagement Closure
+	 *     [ ] Managers - Change Repositories 
+	 *     [ ] Tests - Add PluginManagement Closure
+	 *     [ ] Tests - Change Repositories
+	 *     Isolated: 
+	 *         [ ] Add all managers to test dependencies
+	 * Test:
+	 * [ ] Build Manager
+	 * [ ] Build Tests
+	 */
+	
+	
 
     abstract protected IGenericEcosystem getEcosystem();
 
