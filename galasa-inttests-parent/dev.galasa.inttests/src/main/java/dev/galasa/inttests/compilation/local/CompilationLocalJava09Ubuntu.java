bd0365f734cb2e736b484de429b591ddc76f4e97
Matthew Chivers <matthewchivers@outlook.com>
2021-07-15 12:02:20 +0100

    Added local regular test templates
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

---
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/local/CompilationLocalJava09Ubuntu.java
@@ -0,0 +1,34 @@
+package dev.galasa.inttests.compilation.local;
+
+import dev.galasa.Test;
+import dev.galasa.TestAreas;
+import dev.galasa.galasaecosystem.IGenericEcosystem;
+import dev.galasa.galasaecosystem.ILocalEcosystem;
+import dev.galasa.galasaecosystem.LocalEcosystem;
+import dev.galasa.inttests.compilation.AbstractCompilationLocal;
+import dev.galasa.java.JavaVersion;
+import dev.galasa.java.ubuntu.IJavaUbuntuInstallation;
+import dev.galasa.java.ubuntu.JavaUbuntuInstallation;
+import dev.galasa.linux.ILinuxImage;
+import dev.galasa.linux.LinuxImage;
+import dev.galasa.linux.OperatingSystem;
+
+@Test
+@TestAreas({"compilation","localecosystem","java09","ubuntu"})
+public class CompilationLocalJava09Ubuntu extends AbstractCompilationLocal {
+
+    @LocalEcosystem(linuxImageTag = "PRIMARY")
+    public ILocalEcosystem ecosystem;
+    
+    @LinuxImage(operatingSystem = OperatingSystem.ubuntu)
+    public ILinuxImage linuxImage;
+    
+    @JavaUbuntuInstallation(javaVersion = JavaVersion.v9)
+    public IJavaUbuntuInstallation java;
+
+    @Override
+    protected IGenericEcosystem getEcosystem() {
+        return this.ecosystem;
+    }
+
+}
---
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/local/CompilationLocalJava10Ubuntu.java
@@ -0,0 +1,34 @@
+package dev.galasa.inttests.compilation.local;
+
+import dev.galasa.Test;
+import dev.galasa.TestAreas;
+import dev.galasa.galasaecosystem.IGenericEcosystem;
+import dev.galasa.galasaecosystem.ILocalEcosystem;
+import dev.galasa.galasaecosystem.LocalEcosystem;
+import dev.galasa.inttests.compilation.AbstractCompilationLocal;
+import dev.galasa.java.JavaVersion;
+import dev.galasa.java.ubuntu.IJavaUbuntuInstallation;
+import dev.galasa.java.ubuntu.JavaUbuntuInstallation;
+import dev.galasa.linux.ILinuxImage;
+import dev.galasa.linux.LinuxImage;
+import dev.galasa.linux.OperatingSystem;
+
+@Test
+@TestAreas({"compilation","localecosystem","java10","ubuntu"})
+public class CompilationLocalJava10Ubuntu extends AbstractCompilationLocal {
+
+    @LocalEcosystem(linuxImageTag = "PRIMARY")
+    public ILocalEcosystem ecosystem;
+    
+    @LinuxImage(operatingSystem = OperatingSystem.ubuntu)
+    public ILinuxImage linuxImage;
+    
+    @JavaUbuntuInstallation(javaVersion = JavaVersion.v10)
+    public IJavaUbuntuInstallation java;
+
+    @Override
+    protected IGenericEcosystem getEcosystem() {
+        return this.ecosystem;
+    }
+
+}
