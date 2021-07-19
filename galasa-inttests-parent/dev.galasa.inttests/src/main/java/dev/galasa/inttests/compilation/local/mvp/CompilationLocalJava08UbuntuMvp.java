270fecd2c25989d7a1d2bf9f0a0d97bdce4af0bf
Matthew Chivers <matthewchivers@outlook.com>
2021-07-19 13:09:50 +0100

    Added methods to test offline compilation for MVP zip
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/local/mvp/CompilationLocalJava08UbuntuMvp.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/local/mvp/CompilationLocalJava08UbuntuMvp.java
@@ -1,12 +1,15 @@
 package dev.galasa.inttests.compilation.local.mvp;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import dev.galasa.Test;
 import dev.galasa.TestAreas;
 import dev.galasa.galasaecosystem.IGenericEcosystem;
 import dev.galasa.galasaecosystem.ILocalEcosystem;
 import dev.galasa.galasaecosystem.IsolationInstallation;
 import dev.galasa.galasaecosystem.LocalEcosystem;
-import dev.galasa.inttests.compilation.AbstractCompilationLocal;
+import dev.galasa.inttests.compilation.AbstractCompilationLocalZip;
 import dev.galasa.java.JavaVersion;
 import dev.galasa.java.ubuntu.IJavaUbuntuInstallation;
 import dev.galasa.java.ubuntu.JavaUbuntuInstallation;
@@ -16,7 +19,7 @@ import dev.galasa.linux.OperatingSystem;
 
 @Test
 @TestAreas({"compilation","localecosystem","java08","ubuntu","mvp"})
-public class CompilationLocalJava08UbuntuMvp extends AbstractCompilationLocal {
+public class CompilationLocalJava08UbuntuMvp extends AbstractCompilationLocalZip {
 
     @LocalEcosystem(linuxImageTag = "PRIMARY", isolationInstallation = IsolationInstallation.Mvp)
     public ILocalEcosystem ecosystem;
@@ -26,6 +29,29 @@ public class CompilationLocalJava08UbuntuMvp extends AbstractCompilationLocal {
     
     @JavaUbuntuInstallation(javaVersion = JavaVersion.v8)
     public IJavaUbuntuInstallation java;
+
+    @Override
+    protected void makeChanges(Path simplatformParent) throws IOException {
+		renameFiles(simplatformParent);
+		changeAllPrefixes(simplatformParent);
+		
+		Path managerBuildGradle = simplatformParent.resolve("dev.galasa.simbank.manager/build.gradle");
+		Path testBuildGradle = simplatformParent.resolve("dev.galasa.simbank.tests/build.gradle");
+		Path parentSettings = simplatformParent.resolve("settings.gradle");
+		
+		// Alter project parent
+		addPluginManagementRepo(parentSettings);
+		
+		// Alter manager project
+		updateMavenRepo(managerBuildGradle); 
+		addDependencyConstraints(managerBuildGradle);
+		
+		// Alter test project
+		updateMavenRepo(testBuildGradle);
+		addDependencyConstraints(testBuildGradle);
+		addImplementationConstraints(testBuildGradle);
+		
+	}
 
     @Override
     protected IGenericEcosystem getEcosystem() {
