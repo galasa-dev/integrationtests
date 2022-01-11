9f5afbff0c9f25a7c36e7ddb7db61282bb5e52e9
JansenGSY <keesjansen@Keess-MacBook-Pro.local>
2022-01-11 08:54:10 +0000

    removed reliance on exit codes
    Signed-off-by: JansenGSY <keesjansen@Keess-MacBook-Pro.local>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/docker/AbstractDocker.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/docker/AbstractDocker.java
@@ -1,27 +1,17 @@
 package dev.galasa.inttests.docker;
 
-import org.apache.commons.logging.Log;
-
-import dev.galasa.core.manager.Logger;
 import dev.galasa.ipnetwork.ICommandShell;
 
 public abstract class AbstractDocker {
 	
-	@Logger
-	public Log logger;
-	
 	abstract protected String updatePackageManager(ICommandShell shell) throws Exception;
 	
 	protected boolean isDockerInstalled(ICommandShell shell) throws Exception {
-		String res = shell.issueCommand("docker -v; echo $?");	
-		// Regex is splitting the string on new line characters. 
-		// As of Java 8 \R pattern will match any Unicode line breaker.
-		// Explained here: https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
-		String returnCode = res.split("\\R")[1];
-		if(returnCode.equals("0")) {
-			return true;
-		} else {
+		String res = shell.issueCommand("docker -v");	
+		if(res.contains("no such file or directory")) {
 			return false;
+		} else {
+			return true;
 		}
 	}
 	
@@ -35,15 +25,11 @@ public abstract class AbstractDocker {
 	}
 	
 	protected boolean isMavenInstalled(ICommandShell shell) throws Exception {
-		String res = shell.issueCommand("mvn -v; echo $?");
-		// Regex is splitting the string on new line characters. 
-		// As of Java 8 \R pattern will match any Unicode line breaker.
-		// Explained here: https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
-		String returnCode = res.split("\\R")[1];
-		if(returnCode.equals("0")) {
-			return true;
-		} else {
+		String res = shell.issueCommand("mvn -v");
+		if(res.contains("no such file or directory")) {
 			return false;
+		} else {
+			return true;
 		}
 	}
 }
