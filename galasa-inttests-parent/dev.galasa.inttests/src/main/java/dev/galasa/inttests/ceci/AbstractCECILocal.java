9005608f095caf7e24309f2100ab1256b0b794a6
Jade Carino <jade.carino@ibm.com>
2022-03-10 15:19:34 +0000

    CPS props
    Signed-off-by: Jade Carino <jade.carino@ibm.com>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/ceci/AbstractCECILocal.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/ceci/AbstractCECILocal.java
@@ -21,11 +21,15 @@ public abstract class AbstractCECILocal {
 		
 		// Properties for provisioning a CICS Region
         getEcosystem().setCpsProperty("cicsts.provision.type", getEcosystem().getHostCpsProperty("cicsts", "provision", "type"));
-        getEcosystem().setCpsProperty("cicsts.default.version", getEcosystem().getHostCpsProperty("cicsts", "default", "version"));
+        String cicstsDefaultVersion = getEcosystem().getHostCpsProperty("cicsts", "default", "version");
+        getEcosystem().setCpsProperty("cicsts.default.version", cicstsDefaultVersion);
         getEcosystem().setCpsProperty("cicsts.default.logon.initial.text", getEcosystem().getHostCpsProperty("cicsts", "default.logon", "text", "initial"));
         getEcosystem().setCpsProperty("cicsts.default.logon.gm.text", getEcosystem().getHostCpsProperty("cicsts", "default.logon", "text", "gm"));
         // Properties for SEM
-        getEcosystem().setCpsProperty("sem.internal.version." + getEcosystem().getHostCpsProperty("cicsts", "default", "version"), getEcosystem().getHostCpsProperty("sem", "internal", getEcosystem().getHostCpsProperty("cicsts", "default", "version"), "version"));
+        String semInternalVersion = getEcosystem().getHostCpsProperty("sem", "internal", cicstsDefaultVersion, "version");
+        getEcosystem().setCpsProperty("sem.internal.version." + cicstsDefaultVersion, semInternalVersion);
+        String semExternalVersion = getEcosystem().getHostCpsProperty("sem", "external", semInternalVersion, "version");
+        getEcosystem().setCpsProperty("sem.external.version." + semInternalVersion, semExternalVersion);
         getEcosystem().setCpsProperty("sem.model.url", getEcosystem().getHostCpsProperty("sem", "model", "url"));
         getEcosystem().setCpsProperty("sem.model.ports", getEcosystem().getHostCpsProperty("sem", "model", "ports"));
         getEcosystem().setCpsProperty("sem.model.applids", getEcosystem().getHostCpsProperty("sem", "model", "applids"));
@@ -33,6 +37,7 @@ public abstract class AbstractCECILocal {
         getEcosystem().setCpsProperty("zosprogram.cobol." + getZosImage().getImageID() + ".dataset.prefix", getEcosystem().getHostCpsProperty("zosprogram", "cobol", "dataset.prefix", getZosImage().getImageID()));
         getEcosystem().setCpsProperty("zosprogram.le." + getZosImage().getImageID() + ".dataset.prefix", getEcosystem().getHostCpsProperty("zosprogram", "le", "dataset.prefix", getZosImage().getImageID()));  
         getEcosystem().setCpsProperty("zosprogram.cics." + getZosImage().getImageID() + ".dataset.prefix", getEcosystem().getHostCpsProperty("zosprogram", "cics", "dataset.prefix", getZosImage().getImageID()));
+        	
 	}
 
     @Test
